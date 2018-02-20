f0c20333e21dd24fdbae951458e8b80aa46e3906
Justin Yang <justin810929@gmail.com>
2018-02-20 14:45:29 +0800

    upload
    upload

---
+++ js/all.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.baglist-open').hover(function() {
+		/* Stuff to do when the mouse enters the element */
+		$('.baglist').css('diplay','inline-block');
+	}, function() {
+		/* Stuff to do when the mouse leaves the element */
+	});
+});
